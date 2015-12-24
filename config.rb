5dd483e9ca269e415c71aeae84e4619debdfa859
Fernando Moreira <nandomoreira.me@gmail.com>
2015-12-24 14:53:57 -0200

    first commit

---
+++ config.rb
@@ -0,0 +1,8 @@
+require 'compass/import-once/activate'
+
+http_path = "/"
+css_dir = "test"
+sass_dir = "sass"
+output_style = :expanded #or :nested or :compact or :compressed
+relative_assets = false
+line_comments = false
